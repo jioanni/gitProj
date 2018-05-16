92dc59ad14a05aa73b5f6f5420c5cef6027e6500
Jason Ioannides <jason.ioannides@gmail.com>
2018-05-16 14:14:48 -0400

    1

--- api/git.js
+++ api/git.js
@@ -8,8 +8,8 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
     let added;
     let modified;
-    req.body.commits.added.length > 0 ?  added = req.body.commits.added : []
-    req.body.commits.modified.length > 0 ? modified = req.body.commits.added : []
+    req.body.commits[0].added.length > 0 ?  added = req.body.commits.added : []
+    req.body.commits[0].modified.length > 0 ? modified = req.body.commits.added : []
     const updates = added.concat(modified)
     res.send(updates)
 })
