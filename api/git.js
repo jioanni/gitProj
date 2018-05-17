5550297a15fdb560d488ff5b683b5d64b50bce55
Jason Ioannides <jason.ioannides@gmail.com>
2018-05-17 13:53:17 -0400

    functional. 1 deprecation warning.

--- api/git.js
+++ api/git.js
@@ -3,6 +3,7 @@ const functions = require('../functions')
 const arrayCleaner = functions.arrayCleaner
 const commitConsolidator = functions.commitConsolidator
 const requester = functions.requester
+const fileWriter = functions.fileWriter
 
 
 router.get('/', (req, res, next) => {
@@ -10,11 +11,14 @@ router.get('/', (req, res, next) => {
     res.send("Boom goes the dynamite.")
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     let body = commitConsolidator(req.body.commits);
     let result = arrayCleaner(body)
-    let results = requester(result)
+    let results = await requester(result)
+    console.log(results, " RESULTS STUFFS")
+    fileWriter(results)
     res.send(results)                       //when this route gets hit with the post from the git webhook, it generates our data.
+    
 })
 
 module.exports = router
